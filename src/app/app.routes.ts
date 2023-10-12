49435c2a6d47013e1eef5222257539fae4b7dc5b
tarcisi0neto <tarcisioxsn@gmail.com>
2023-10-12 14:50:43 -0300

    set config project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Route } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '',
+        component: NxWelcomeComponent,
+        pathMatch: 'full',
+      },
+      {
+        path: 'products',
+        loadComponent: () => import('products').then((m) => m.ProductsComponent),
+      },
+      {
+        path: 'orders',
+        loadComponent: () => import('orders').then((m) => m.OrdersComponent),
+      },
+]
